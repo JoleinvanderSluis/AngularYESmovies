a9443ebbc7b5b36c0b399865540ec2dc78c3c4c2
Suzanne <suzanne.veenhouwer@hotmail.com>
2019-05-20 12:02:12 +0200

    merge with origin develop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { YesProfileListComponent } from './components/yes-profile-list/yes-profile-list.component';
 import { ShowYesProfileByIdComponent } from './components/show-yes-profile-by-id/show-yes-profile-by-id.component';
+import { GenresComponent } from './components/genres/genres.component';
+import { RatingsComponent } from './components/ratings/ratings.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path: 'showYesProfileById/:id',
     component: ShowYesProfileByIdComponent
+  },
+  {
+    path: 'genres',
+    component: GenresComponent
+  },
+  {
+    path: 'ratings',
+    component: RatingsComponent
   }
 ];
 
