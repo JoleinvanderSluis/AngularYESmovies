c1dfdaa885f92a5508590c13e26e7345d557698e
unknown <joleinvandersluis@gmail.com>
2019-05-22 14:51:29 +0200

    adding favourite genres to profile is now possible!

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { YesProfile } from '../model/yes-profile';
+import { Genre } from '../model/genre';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,20 @@ export class ProfileService {
 
   constructor(private httpClient: HttpClient) { }
 
+  private url: string = "http://localhost:8080/api";
+  private endpoint: string = "yesProfiles";
+
   createYesProfile( yesProfile: YesProfile){
-    console.log("In createYesProfile" + yesProfile)
-    return this.httpClient.post('http://localhost:8080/api/yesProfiles',yesProfile);
+    console.log("In profile service createYesProfile" + yesProfile)
+    return this.httpClient.post(`${this.url}/${this.endpoint}`,yesProfile);
+  }
+
+  addFavouriteGenre(yesProfileId: number, genreId: number){
+    console.log("In profile service addFavouriteGenre" + genreId)
+    return this.httpClient.put(`${this.url}/${this.endpoint}/${yesProfileId}/addPreferredGenre/${genreId}`,genreId);
   }
 
   getYesProfileList() {
-    return this.httpClient.get('http://localhost:8080/api/yesProfiles');
+    return this.httpClient.get(`${this.url}/${this.endpoint}`);
   }
-
-  findYesProfileById(id: Number){
-    return this.httpClient.get('http://localhost:8080/api/yesProfiles/${id}');
-  } 
 }
